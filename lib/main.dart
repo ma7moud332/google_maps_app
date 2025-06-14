d2028ccc494cf73e6bc92d0937f6b77afd1c4f4a
ma7moud332 <119896210+ma7moud332@users.noreply.github.com>
2025-06-15 00:34:49 +0300

    Refactor Google Maps integration by creating CustomGoogleMap widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_app/widgets/custom_google_map.dart';
 
 void main() {
   runApp(const GoogleMapsApp());
@@ -12,15 +12,7 @@ class GoogleMapsApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        body: GoogleMap(
-          mapType: MapType.hybrid,
-          initialCameraPosition: const CameraPosition(
-            target: LatLng(37.7749, -122.4194), // San Francisco coordinates
-            zoom: 10.0,
-          ),
-        ),
-      ),
+      home: CustomGoogleMap(),
     );
   }
 }
