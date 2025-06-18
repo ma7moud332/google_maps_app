f5c47f0a28418612330a92c66f6c43ebbe635ea7
ma7moud332 <119896210+ma7moud332@users.noreply.github.com>
2025-06-19 02:09:11 +0300

    Add location service and permission checks in CustomGoogleMap widget

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_app/models/place_model.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class CustomGoogleMap extends StatefulWidget {
   const CustomGoogleMap({super.key});
@@ -12,6 +13,7 @@ class CustomGoogleMap extends StatefulWidget {
 class _CustomGoogleMapState extends State<CustomGoogleMap> {
   late CameraPosition initialCameraPosition;
 
+  late Location location;
   @override
   void initState() {
     initialCameraPosition = const CameraPosition(
@@ -19,14 +21,22 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
       zoom: 12,
     );
     initMarkers();
+    location = Location();
+    checkAndRequestLocationService();
     super.initState();
   }
 
+  late GoogleMapController googleMapController;
   Set<Marker> markers = {};
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
       markers: markers,
+      onMapCreated: (controller) {
+        googleMapController = controller;
+
+        location.onLocationChanged.listen((locationData) {});
+      },
       initialCameraPosition: initialCameraPosition,
     );
   }
@@ -48,5 +58,28 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
         .toSet();
     markers.addAll(myMarkers);
     setState(() {});
+  }
+
+  void checkAndRequestLocationService() async {
+    var isServiceEnabled = await location.serviceEnabled();
+
+    if (!isServiceEnabled) {
+      isServiceEnabled = await location.requestService();
+      if (!isServiceEnabled) {
+        // Handle the case where the user did not enable the location service
+      }
+    }
+    checkAndRequestLocationPermission();
+  }
+
+  void checkAndRequestLocationPermission() async {
+    var permissionStatus = await location.hasPermission();
+
+    if (permissionStatus == PermissionStatus.denied) {
+      permissionStatus = await location.requestPermission();
+      if (permissionStatus != PermissionStatus.granted) {
+        // Handle the case where the user did not grant the location permission
+      }
+    }
   }
 }
